c929064b15827011969d2a3f6496491a3f86e20f
l-t-k <leonkooiman@gmail.com>
2017-11-18 03:19:22 +0100

    Update retrieve.sh

--- root/defaults/retrieve.sh
+++ root/defaults/retrieve.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # uncomment line underneath to update spotweb database every 15 minutes
-# /usr/bin/php /config/www/spotweb/retrieve.php >> /config/log/spotweb/retrieve.log 2>&1
+/usr/bin/php /config/www/spotweb/retrieve.php >> /config/log/spotweb/retrieve.log 2>&1
 echo "Done, retrieving spots!" >> /config/log/spotweb/retrieve.log 2>&1
