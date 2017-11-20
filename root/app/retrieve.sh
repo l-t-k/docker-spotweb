f7bd619364a9ee7bee937888064d09badd3572f3
l-t-k <leonkooiman@gmail.com>
2017-11-20 01:07:02 +0100

    Update retrieve.sh

--- root/app/retrieve.sh
+++ root/app/retrieve.sh
@@ -6,5 +6,8 @@ echo "cronjob running on "$(date)
 echo "Making sure our cache isn't corrupt"
 /usr/bin/php /config/www/spotweb/bin/check-cache.php
 
+echo "Making sure our cache is owned by user abc"
+chown -R abc:abc /config/www/spotweb/cache
+
 echo "Running retrieve spots"
 /usr/bin/php /config/www/spotweb/retrieve.php
